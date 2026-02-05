83055798b20615aac2765f701b790cd7dd19813c
Zoggy9 <120832315+Zoggy9@users.noreply.github.com>
2026-02-05 07:29:03 -0500

    Update classroom.js

--- cloneclass/classroom.js
+++ cloneclass/classroom.js
@@ -162,7 +162,7 @@ function renderAnnouncements() {
       '</p><p class="announcement-message">' +
       ann.message
         .replace(/IXL/g, '<a href="/xl-learning.html" target="_blank">IXL</a>')
-        .replace(/textbook/g, '<a href="https://ewufweufc.onrender.com" target="_blank">textbook</a>')+
+        .replace(/textbook/g, '<a href="https://ewufweufc.onrender.com" target="_blank">textbook</a>') +
       "</p></div></div>";
     content.appendChild(div);
   }
